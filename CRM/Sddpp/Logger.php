aba0c2b56def2c3130a41b601234d1700f6fc614
B. Endres <endres@systopia.de>
2021-10-07 20:47:42 +0200

    implemented logger

--- CRM/Sddpp/Logger.php
+++ CRM/Sddpp/Logger.php
@@ -17,15 +17,45 @@ use CRM_Sddpp_ExtensionUtil as E;
  */
 class CRM_Sddpp_Logger
 {
-    public static function debug($log_message) {
-        Civi::log()->debug('[SDDPP] ' . $log_message);
+    public static function shouldLog($log_level)
+    {
+        static $min_log_level = null;
+        if ($min_log_level === null) {
+            $min_log_level = Civi::settings()->get(CRM_Sddpp_Form_Settings::LOG_LEVEL);
+        }
+        switch ($min_log_level) {
+            case 'off':
+                return false;
+
+            case 'debug':
+                return true;
+
+            case 'warning':
+                return $log_level != 'debug';
+
+            case 'error':
+                return $log_level == 'error';
+        }
     }
 
-    public static function warning($log_message) {
-        Civi::log()->warning('[SDDPP] ' . $log_message);
+    public static function debug($log_message)
+    {
+        if (self::shouldLog('debug')) {
+            Civi::log()->debug('[SDDPP] ' . $log_message);
+        }
     }
 
-    public static function error($log_message) {
-        Civi::log()->error('[SDDPP] ' . $log_message);
+    public static function warning($log_message)
+    {
+        if (self::shouldLog('warning')) {
+            Civi::log()->warning('[SDDPP] ' . $log_message);
+        }
+    }
+
+    public static function error($log_message)
+    {
+        if (self::shouldLog('error')) {
+            Civi::log()->error('[SDDPP] ' . $log_message);
+        }
     }
 }
